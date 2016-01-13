d67814d6c73393b27a614d4370c1969186f5cf9e
buptrobin <robinwang@yosemitecloud.com>
2016-01-13 12:53:50 +0800

    HappyNumber

--- src/com/robin/exam/CodeExamMain.java
+++ src/com/robin/exam/CodeExamMain.java
@@ -7,7 +7,9 @@ public class CodeExamMain {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!!!");
+		//System.out.println("Hello World!!!");
+		
+		HappyNumber.Test();
 	}
 
 }
--- src/com/robin/exam/HappyNumber.java
+++ src/com/robin/exam/HappyNumber.java
@@ -1,5 +1,52 @@
 package com.robin.exam;
 
+import java.util.HashSet;
+
+/*
+ * Write an algorithm to determine if a number is happy.
+ * A happy number is a number defined by the following process: 
+ * Starting with any positive integer, replace the number by the sum of the squares of its digits, 
+ * and repeat the process until the number equals 1 (where it will stay), 
+ * or it loops endlessly in a cycle which does not include 1. 
+ * Those numbers for which this process ends in 1 are happy numbers.
+ */
 public class HappyNumber {
+	public static void Test()
+	{
+		HappyNumber hn = new HappyNumber();
+		System.out.println(true == hn.isHappy(333));
+	}
 	
+    /**
+     * @param n an integer
+     * @return true if this is a happy number or false
+     */
+    public boolean isHappy(int n) {
+		if(n == 1) { return true; }
+		if(n <= 0) { return false; }
+		
+	    HashSet<Integer> hs = new HashSet<Integer>();
+	    int m = n;
+	    
+	    while(!hs.contains(m))
+	    {
+	    	if( m == 1 ) return true;
+	    	
+	    	hs.add(m);
+	    	
+	    	int sum = 0;
+
+	    	while(m > 0)
+	    	{
+		    	int k = m % 10;
+		    	sum += k * k;
+		    	m /= 10;	    	
+	    	}
+	    	
+	    	m = sum;
+	    }
+	    
+	    return false;
+    }
 }
+
